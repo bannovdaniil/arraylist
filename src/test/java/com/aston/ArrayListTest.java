d192c225949880aa78bf81199e3150117707e66e
bannovdaniil <baddan@mail.ru>
2023-09-09 23:53:31 +0300

    init test file

---
+++ src/test/java/com/aston/ArrayListTest.java
@@ -0,0 +1,45 @@
+package com.aston;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListTest {
+
+    @Test
+    void add() {
+
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void set() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void testRemove() {
+    }
+
+    @Test
+    void get() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void sort() {
+    }
+}
